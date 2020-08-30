11d1a8d9b23e17fc93678cab80a17d79d95cb575
Skyteks <Schattenpalkia@googlemail.com>
2020-08-30 19:09:20 +0200

    Add floating point jitter manager

---
+++ Assets/Scripts/Stellar/EndlessManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessManager : MonoBehaviour
+{
+    public float distanceThreshold = 1000f;
+    private List<Rigidbody> physicsObjects;
+
+    public event System.Action PostFloatingOriginUpdate;
+
+    void Awake()
+    {
+        physicsObjects = new List<Rigidbody>(FindObjectsOfType<Rigidbody>());
+    }
+
+    void LateUpdate()
+    {
+        UpdateFloatingOrigin();
+        PostFloatingOriginUpdate?.Invoke();
+    }
+
+    void UpdateFloatingOrigin()
+    {
+        Vector3 originOffset = Camera.main.transform.position;
+        float distanceFromOrigin = originOffset.magnitude;
+
+        if (distanceFromOrigin > distanceThreshold)
+        {
+            foreach (Rigidbody rigid in physicsObjects)
+            {
+                rigid.position -= originOffset;
+            }
+        }
+    }
+}
